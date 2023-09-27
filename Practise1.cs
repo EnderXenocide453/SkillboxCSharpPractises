7be707fddf55616cf06b822a9a0b07f601349d7d
EnderXenocide453 <lakssterius@gmail.com>
2023-09-27 11:01:36 +0300

    Первая практическая работа

---
+++ Practise1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillboxPractises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+
+            //Задание 2
+            Console.Write("Hello ");
+            Console.Write("World");
+            Console.Write("!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
